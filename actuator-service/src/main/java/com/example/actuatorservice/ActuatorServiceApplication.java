559969d45ab9b612ab3195e33be982caa30c9732
FrankieRM <frodriguezm17@gmail.com>
2020-04-11 11:46:04 -0500

    Building a RESTful Web Service with Spring Boot Actuator

---
+++ actuator-service/src/main/java/com/example/actuatorservice/ActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuatorservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ActuatorServiceApplication {
+
+    public static void main(String[] args) {
+        run(ActuatorServiceApplication.class, args);
+    }
+}
---
+++ actuator-service/src/main/java/com/example/actuatorservice/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.actuatorservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+
+    private final long id;
+    private final String content;
+}
